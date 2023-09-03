1a32254293be08960fa0f5f8e54be889f94c45cf
Vladimir Khromov <vlad2aria@mail.ru>
2023-09-03 14:11:03 +0300

    Chapter 04

--- util/chapter_04.py
+++ util/chapter_04.py
@@ -1,9 +1,12 @@
 from aiohttp import ClientSession
-
+from asyncio import sleep
 from util import async_timed
 
 
 @async_timed()
-async def fetch_status(session: ClientSession, url: str) -> int:
+async def fetch_status(session: ClientSession,
+                       url: str,
+                       delay: int = 0) -> int:
+    await sleep(delay)
     async with session.get(url) as result:
         return result.status
